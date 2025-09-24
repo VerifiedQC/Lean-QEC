1c8f0c274d829d6cc231343a4dda25ffeddf7a92
mehatamm <mati@ehatamm.com>
2025-09-24 13:50:27 -0400

    define I matrix

--- ATPTest/Basic.lean
+++ ATPTest/Basic.lean
@@ -1,3 +1,19 @@
+import QuantumInfo
+import ClassicalInfo
+import LeanCopilot
 import ClassicalInfo.Capacity
 
 #check Code
+
+#check Qubit
+
+open Qubit
+
+def I : 𝐔[Qubit] :=
+  ⟨!![Complex.I, 0; 0, Complex.I], by constructor <;> matrix_expand⟩
+
+
+@[simp]
+theorem X_Y_flip: X * Y = I * Z := by {
+  matrix_expand [I, Z, X, Y]
+}
