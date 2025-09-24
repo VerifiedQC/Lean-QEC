ffeb7bfd62b64873ef05fa9edf6b492f1cafd736
mehatamm <mati@ehatamm.com>
2025-09-23 22:46:26 -0400

    initial commit

---
+++ ATPTest/Basic.lean
@@ -0,0 +1,3 @@
+import ClassicalInfo.Capacity
+
+#check Code
---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open System Lake DSL
+
+package «ATP_Test»{
+}
+
+require quantumInfo from git "https://github.com/Timeroot/Lean-QuantumInfo"
+
+
+@[default_target]
+lean_lib ATPTest
