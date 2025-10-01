b8681c13bef09da642c927c0aab94a82837a4273
mehatamm <mati@ehatamm.com>
2025-10-01 13:24:44 -0400

    use of_kraus_CPTPMap

--- ATPTest/Pauli_Channels.lean
+++ ATPTest/Pauli_Channels.lean
@@ -4,11 +4,11 @@ import ATPTest.Basic
 -- First define single-qubit bit-flip channel
 open Qubit
 
-
+noncomputable section
 variable (U : 𝐔[Qubit])
 
 
-noncomputable def kraus_unitary (p : ℝ) (i : Qubit): Matrix Qubit Qubit ℂ :=
+def kraus_unitary (p : ℝ) (i : Qubit): Matrix Qubit Qubit ℂ :=
   match i with
   | 0 => √(1-p) • 1
   | 1 => √p • U
@@ -17,7 +17,7 @@ noncomputable def kraus_unitary (p : ℝ) (i : Qubit): Matrix Qubit Qubit ℂ :=
 
 @[simp] lemma kraus_unitary_1 : kraus_unitary U p 1 = √p • U := by rfl
 
-noncomputable def kraus_bit_flip := kraus_unitary X
+def kraus_bit_flip := kraus_unitary X
 
 @[simp] lemma kraus_bit_flip_0 : kraus_bit_flip p 0 = √(1 - p) • 1 := by
   rfl
@@ -25,7 +25,7 @@ noncomputable def kraus_bit_flip := kraus_unitary X
 @[simp] lemma kraus_bit_flip_1 : kraus_bit_flip p 1 = √p • X := by
   rfl
 
-noncomputable def kraus_dephasing := kraus_unitary Z
+def kraus_dephasing := kraus_unitary Z
 
 @[simp] lemma kraus_dephasing_0 : kraus_dephasing p 0 = √(1 - p) • 1 := by
   rfl
@@ -37,43 +37,44 @@ lemma unitary_ct : (U: Matrix Qubit Qubit ℂ).conjTranspose * (U: Matrix Qubit 
   rw [←Matrix.star_eq_conjTranspose]
   simp only [SetLike.coe_mem, unitary.star_mul_self_of_mem]
 
+def unitary_p_channel (p : ℝ) (hp1 : 0 ≤ p) (hp2 : 0 ≤ 1 - p) := CPTPMap.of_kraus_CPTPMap (kraus_unitary U p) (
+  by
+    simp [unitary_ct, smul_smul, ←add_smul, Real.mul_self_sqrt hp1, Real.mul_self_sqrt hp2]
+)
 
-noncomputable def unitary_channel (p : ℝ) (hp1 : 0 ≤ p) (hp2 : 0 ≤ 1 - p) : CPTPMap Qubit Qubit ℂ where
-  toLinearMap := MatrixMap.of_kraus (kraus_unitary U p) (kraus_unitary U p)
-  cp := MatrixMap.IsCompletelyPositive.of_kraus_isCompletelyPositive (kraus_unitary U p)
-  TP := by
-    apply MatrixMap.IsTracePreserving.of_kraus_isTracePreserving (kraus_unitary U p) (kraus_unitary U p)
-    simp
-    rewrite [smul_smul, Real.mul_self_sqrt hp2, smul_smul, Real.mul_self_sqrt hp1]
-    rewrite [unitary_ct, ←add_smul, sub_add_cancel, one_smul]
-    rfl
+def bit_flip_channel (p : ℝ) (hp1 : 0 ≤ p) (hp2 : 0 ≤ 1 - p) := unitary_p_channel Z p hp1 hp2
 
-noncomputable def bit_flip_channel (p : ℝ) (hp1 : 0 ≤ p) (hp2 : 0 ≤ 1 - p) := unitary_channel Z p hp1 hp2
-
-noncomputable def kraus_pauli (p1 p2 p3 p4 : ℝ) (i : Fin 4): Matrix Qubit Qubit ℂ :=
+def kraus_pauli (p1 p2 p3 p4 : ℝ) (i : Fin 4): Matrix Qubit Qubit ℂ :=
   match i with
   | 0 => √p1 • 1
   | 1 => √p2 • X
   | 2 => √p3 • Y
   | 3 => √p4 • Z
 
-noncomputable def pauli_channel (p1 p2 p3 p4 : ℝ) (hp1: 0 ≤ p1) (hp2: 0 ≤ p2) (hp3: 0 ≤ p3) (hp4: 0 ≤ p4)
+def pauli_channel (p1 p2 p3 p4 : ℝ) (hp1: 0 ≤ p1) (hp2: 0 ≤ p2) (hp3: 0 ≤ p3) (hp4: 0 ≤ p4)
 (hp : p1 + p2 + p3 + p4 = 1)
- : CPTPMap Qubit Qubit ℂ where
-  toLinearMap := MatrixMap.of_kraus (kraus_pauli p1 p2 p3 p4) (kraus_pauli p1 p2 p3 p4)
-  cp := MatrixMap.IsCompletelyPositive.of_kraus_isCompletelyPositive (kraus_pauli p1 p2 p3 p4)
-  TP := by
-    apply MatrixMap.IsTracePreserving.of_kraus_isTracePreserving (kraus_pauli p1 p2 p3 p4) (kraus_pauli p1 p2 p3 p4)
-    simp [Fin.sum_univ_four, kraus_pauli, unitary_ct, smul_smul, Real.mul_self_sqrt hp1,
+ := CPTPMap.of_kraus_CPTPMap (kraus_pauli p1 p2 p3 p4)
+ ( by
+  simp [Fin.sum_univ_four, kraus_pauli, unitary_ct, smul_smul, Real.mul_self_sqrt hp1,
     Real.mul_self_sqrt hp2, Real.mul_self_sqrt hp3, Real.mul_self_sqrt hp4, ←add_smul, hp]
+ )
 
 
 
-noncomputable def kraus_depolarizing (p : ℝ) (i : Fin 4): Matrix Qubit Qubit ℂ := kraus_pauli (1-p) (p/3) (p/3) (p/3) i
 
-noncomputable def depolarizing_channel (p : ℝ) (hp1: 0 ≤ p) (hp2 : 0 ≤ 1-p) :=
+
+def kraus_depolarizing (p : ℝ) (i : Fin 4): Matrix Qubit Qubit ℂ := kraus_pauli (1-p) (p/3) (p/3) (p/3) i
+
+def depolarizing_channel (p : ℝ) (hp1: 0 ≤ p) (hp2 : 0 ≤ 1-p) :=
 pauli_channel (1-p) (p/3) (p/3) (p/3) hp2 (by linarith) (by linarith) (by linarith)
 
+--reason about the kraus operators of channel products
+
+
+
+
+
+def two_qubit_bit_flip (p : ℝ) (hp1 : 0 ≤ p) (hp2 : 0 ≤ 1-p) := (bit_flip_channel p hp1 hp2) ⊗ₖ (bit_flip_channel p hp1 hp2)
 
 def of_kraus_prob_unitary {n : ℕ} (pfun : Fin n → ℝ) (hpfun1 : ∀ i, 0 ≤ pfun i) (hpfun2 : ∀ i, 0 ≤ 1 - pfun i)
 : CPTPMap Qubit Qubit ℂ := sorry
