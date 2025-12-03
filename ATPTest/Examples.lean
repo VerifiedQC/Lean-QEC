fe46eea640b1bd0b9c1e728238be26d6e1d45841
mehatamm <mati@ehatamm.com>
2025-12-03 17:57:37 -0500

    prove stabilizer membership for three qub code

--- ATPTest/Examples.lean
+++ ATPTest/Examples.lean
@@ -24,6 +24,21 @@ lemma unitary_kron_assoc {U₁ U₂ U₃ : 𝐔ₙ[1]}  :
   ring_nf
   fin_cases i <;> fin_cases j <;> rfl
 
+lemma PState.kron_val (n₁ n₂) {ψ₁ : PState n₁} {ψ₂ : PState n₂} {x : BitVec (n₁ + n₂)} :
+  (ψ₁ ⊗ₚ ψ₂) x = (ψ₁ (bits_cat.symm x).1) * (ψ₂ (bits_cat.symm x).2) := by
+  simp [PState.kron, ket_prod, toMat, normalized_kron]
+  rfl
+
+lemma BitVec.extractLsb'_iter {a b c d} {x : BitVec d} (hbc : b ≤ c):
+  BitVec.extractLsb' a b (BitVec.extractLsb' a c x) = BitVec.extractLsb' a b x := by
+  sorry
+
+lemma PState.kron_assoc {ψ₁ ψ₂ ψ₃ : PState 1} : (ψ₁ ⊗ₚ ψ₂) ⊗ₚ ψ₃ = ψ₁ ⊗ₚ (ψ₂ ⊗ₚ ψ₃) := by
+  ext x
+  rw [@PState.kron_val 2 1, PState.kron_val]
+  rw [@PState.kron_val 1 2, PState.kron_val]
+  rw [mul_assoc]
+  fin_cases x <;> rfl
 
 lemma PState.apply_orth {n : ℕ} {ψ φ : PState n} (h_orth : ψ.orth φ) {U : 𝐔ₙ[n]} :
   (ψ.apply U).orth (φ.apply U) := by
@@ -33,7 +48,7 @@ lemma PState.sum_apply {n : ℕ} {ψ φ : PState n} {a b : ℂ} (hab : ‖a‖^2
   (PState.sum ψ φ a b hab h_orth).apply U = PState.sum (ψ.apply U) (φ.apply U) a b hab (PState.apply_orth h_orth) := by admit
 
 @[simp]
-lemma Pstate.apply_id {n : ℕ} {ψ : PState n} : ψ.apply 1 = ψ := by simp
+lemma PState.apply_id {n : ℕ} {ψ : PState n} : ψ.apply 1 = ψ := by simp
 
 @[simp]
 lemma qub_zero_Z : qub_zero.apply pZ = qub_zero := by admit
@@ -51,33 +66,91 @@ abbrev IZZ_pg := fold1 gt0_3 ![Pauli_I, Pauli_Z, Pauli_Z]
 abbrev ZZI_pg := fold1 gt0_3 ![Pauli_Z, Pauli_Z, Pauli_I]
 abbrev stab := QCode.stabilizers gt0_3 three_qubit_encode
 
+lemma phase_id_coe : (↑(1 : pgroup_phases) : phase).z = 1 := by
+  rfl
+
+lemma Ket.mul_phase_id {k : Type*} [Fintype k] {ψ : Ket k} : ψ.phase_mul phase_id = ψ := by simp [Ket.phase_mul]
+
 lemma IZZ_in_stab :
   (fold1 gt0_3 ![Pauli_I, Pauli_Z, Pauli_Z]).1 ∈
   QCode.stabilizers gt0_3 three_qubit_encode := by
-  /-
-  intro ψ
-  rewrite [three_qubit_encode_correct]
-  unfold stabilizes
-  rw [PState.sum_apply]
-  have hz : ((qub_zero ⊗ₚ qub_zero ⊗ₚ qub_zero).apply (1 ⊗ₙ pZ ⊗ₙ pZ) : PState 3) = ((qub_zero ⊗ₚ qub_zero ⊗ₚ qub_zero) : PState 3) := by
-    rw [kron_mul_kron, kron_mul_kron]
-    admit
-  have ho: (qub_one ⊗ₚ qub_one ⊗ₚ qub_one).apply (1 ⊗ₙ pZ ⊗ₙ pZ) = ((qub_one ⊗ₚ qub_one ⊗ₚ qub_one) : PState 3) := by
-    admit
-  simp_rw [hz, ho]
-  -/
-  admit
-  /- TODO FIX AND RESTORE -/
-  /-
-  rw [mem_PauliGroup_iff]
-  refine' ⟨![Pauli_Z, Pauli_Z, Pauli_I],⟨pgphase_1, _⟩⟩
-  simp [-phase_id, unitary_n_nkron, Pauli_I, Pauli_Z]
-  rw [unitary_kron_assoc]
-  -/
+  constructor
+  · intro ψ
+    rw [three_qubit_encode_correct]
+    unfold stabilizes
+    rw [PState.sum_apply]
+    have hz : (qub_zero ⊗ₚ qub_zero ⊗ₚ qub_zero).apply ↑(fold1 gt0_3 ![Pauli_I, Pauli_Z, Pauli_Z]) = (qub_zero ⊗ₚ qub_zero ⊗ₚ qub_zero)
+    · unfold fold1 foldPauli
+      simp [-PState.apply]
+      unfold fold fold_aux
+      simp [-PState.apply, U_phase, phase_id_coe, unitary_n_nkron]
+      rw [unitary_kron_assoc, kron_mul_kron, kron_mul_kron]
+      simp_rw [Pauli_Z, Pauli_I, qub_zero_Z]
+      simp
+    have ho : (qub_one ⊗ₚ qub_one ⊗ₚ qub_one).apply ↑(fold1 gt0_3 ![Pauli_I, Pauli_Z, Pauli_Z]) = (qub_one ⊗ₚ qub_one ⊗ₚ qub_one)
+    · unfold fold1 foldPauli
+      simp [-PState.apply]
+      unfold fold fold_aux
+      simp [-PState.apply, U_phase, phase_id_coe, unitary_n_nkron]
+      rw [unitary_kron_assoc, kron_mul_kron, kron_mul_kron]
+      simp_rw [Pauli_Z, Pauli_I, qub_one_Z]
+      simp_rw [←PState.kron_assoc, PState.phase_prod_phase]
+      simp [Ket.phase_mul]
+    simp_rw [hz, ho]
+  simp only [Finset.coe_mem]
 
-
-lemma ZIZ_in_stab : (fold1 gt0_3 ![Pauli_Z, Pauli_I, Pauli_Z]).1 ∈ stab := by admit
-lemma ZZI_in_stab : (fold1 gt0_3 ![Pauli_Z, Pauli_Z, Pauli_I]).1 ∈ stab := by admit
+lemma ZIZ_in_stab : (fold1 gt0_3 ![Pauli_Z, Pauli_I, Pauli_Z]).1 ∈ stab := by
+  constructor
+  · intro ψ
+    rw [three_qubit_encode_correct]
+    unfold stabilizes
+    rw [PState.sum_apply]
+    have hz : (qub_zero ⊗ₚ qub_zero ⊗ₚ qub_zero).apply ↑(fold1 gt0_3 ![Pauli_Z, Pauli_I, Pauli_Z]) = (qub_zero ⊗ₚ qub_zero ⊗ₚ qub_zero)
+    · unfold fold1 foldPauli
+      simp [-PState.apply]
+      unfold fold fold_aux
+      simp [-PState.apply, U_phase, phase_id_coe, unitary_n_nkron]
+      rw [unitary_kron_assoc, kron_mul_kron, kron_mul_kron]
+      simp_rw [Pauli_Z, Pauli_I, qub_zero_Z]
+      simp
+    have ho : (qub_one ⊗ₚ qub_one ⊗ₚ qub_one).apply ↑(fold1 gt0_3 ![Pauli_Z, Pauli_I, Pauli_Z]) = (qub_one ⊗ₚ qub_one ⊗ₚ qub_one)
+    · unfold fold1 foldPauli
+      simp [-PState.apply]
+      unfold fold fold_aux
+      simp [-PState.apply, U_phase, phase_id_coe, unitary_n_nkron]
+      rw [unitary_kron_assoc, kron_mul_kron, kron_mul_kron]
+      simp_rw [Pauli_Z, Pauli_I, qub_one_Z]
+      simp_rw [←PState.kron_assoc, PState.apply_id]
+      nth_rewrite 2 [←@Ket.mul_phase_id _ _ qub_one]
+      simp_rw [PState.phase_prod_phase]
+      simp [Ket.phase_mul]
+    simp_rw [hz, ho]
+  simp only [Finset.coe_mem]
+lemma ZZI_in_stab : (fold1 gt0_3 ![Pauli_Z, Pauli_Z, Pauli_I]).1 ∈ stab := by
+  constructor
+  · intro ψ
+    rw [three_qubit_encode_correct]
+    unfold stabilizes
+    rw [PState.sum_apply]
+    have hz : (qub_zero ⊗ₚ qub_zero ⊗ₚ qub_zero).apply ↑(fold1 gt0_3 ![Pauli_Z, Pauli_Z, Pauli_I]) = (qub_zero ⊗ₚ qub_zero ⊗ₚ qub_zero)
+    · unfold fold1 foldPauli
+      simp [-PState.apply]
+      unfold fold fold_aux
+      simp [-PState.apply, U_phase, phase_id_coe, unitary_n_nkron]
+      rw [unitary_kron_assoc, kron_mul_kron, kron_mul_kron]
+      simp_rw [Pauli_Z, Pauli_I, qub_zero_Z]
+      simp
+    have ho : (qub_one ⊗ₚ qub_one ⊗ₚ qub_one).apply ↑(fold1 gt0_3 ![Pauli_Z, Pauli_Z, Pauli_I]) = (qub_one ⊗ₚ qub_one ⊗ₚ qub_one)
+    · unfold fold1 foldPauli
+      simp [-PState.apply]
+      unfold fold fold_aux
+      simp [-PState.apply, U_phase, phase_id_coe, unitary_n_nkron]
+      rw [unitary_kron_assoc, kron_mul_kron, kron_mul_kron]
+      simp_rw [Pauli_Z, Pauli_I, qub_one_Z]
+      simp_rw [PState.phase_prod_phase]
+      simp [Ket.phase_mul]
+    simp_rw [hz, ho]
+  simp only [Finset.coe_mem]
 
 /--
   The Pauli matrices X and I are not equal to each other
