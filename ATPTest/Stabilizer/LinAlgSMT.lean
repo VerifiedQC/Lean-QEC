2871e42a0b9f28f7494d260a56178084f41d653d
mehatamm <mati@ehatamm.com>
2026-02-04 16:51:33 -0500

    mess with unfolding

--- ATPTest/Stabilizer/LinAlgSMT.lean
+++ ATPTest/Stabilizer/LinAlgSMT.lean
@@ -5,18 +5,18 @@ import Mathlib.Data.Finset.Fold
 
 
 variable (e : ℕ → Bool)
-
+/-
 def n : ℕ := 5
 def r₁ : ℕ := 4
 def r₂ : ℕ := 5
-def H_x : Matrix ℕ ℕ Bool := fun _ _ => false
-def H_z : Matrix ℕ ℕ Bool := fun _ _ => true
+def Hx : Matrix ℕ ℕ Bool := fun _ _ => false
+def Hz : Matrix ℕ ℕ Bool := fun _ _ => true
 def wt : ℕ := 3
 def H_x_ker : Matrix ℕ ℕ Bool := fun _ _ => false
 def x_ker_size := 2
 def H_z_ker : Matrix ℕ ℕ Bool := fun _ _ => false
 def z_ker_size := 2
-
+-/
 --don't work with fin at all, just define your matrices so they return 0 on other nats
 --resolved compile time when unfolding matrix defs
 
@@ -50,15 +50,25 @@ def dist_index_le (n r₁ z_ker_size : ℕ) (Hₓ H_z_ker : Matrix ℕ ℕ Bool)
   (is_index_bool I d n) && (nontrivial_bool n x)
     && (mem_ker_bool r₁ n Hₓ x) && (not_mem_rowspace z_ker_size n H_z_ker x)
 
+lemma Finset.fold_range_add_one {β : Type*}
+  {op : β → β → β} [hc : Std.Commutative op] [ha : Std.Associative op] {n : ℕ} {f : ℕ → β} {b : β}
+  : (Finset.range (n+1)).fold op b f = op (f (n+1)) ((Finset.range n).fold op b f) := sorry
+
+/- --
+lemma Finset.fold_range_split {β : Type*}
+  {op : β → β → β} [hc : Std.Commutative op] [ha : Std.Associative op] {n : ℕ} {f : ℕ → β} {b : β}
+  : (Finset.range (2 * n)).fold op b f = (Finset.range n).fold
+-/
+
 open Lean Meta Elab Tactic Syntax
 elab "unfold_range"
  : tactic => do
-  evalTactic (← `(tactic| repeat rw [Finset.range_add_one]))
-  evalTactic (← `(tactic| repeat rw [Finset.fold_insert (by aesop)]))
+  evalTactic (← `(tactic| repeat rw [Finset.fold_range_add_one]))
   evalTactic (← `(tactic| repeat rw [Finset.range_zero]))
   evalTactic (← `(tactic| repeat rw [Finset.fold_empty]))
 
 --before calling:
+set_option maxHeartbeats 0
 elab "prep_smt"
  : tactic => do
   evalTactic (← `(tactic| dsimp))
@@ -85,11 +95,12 @@ elab "prep_smt"
   --evalTactic (← `(tactic| repeat rw [Finset.range_zero, Finset.fold_empty, Bool.xor_false]))
 
 
-
-theorem dist_test : dist_index_le n r₁ z_ker_size H_x H_z_ker 2 := by
+/-
+theorem dist_test : dist_index_le n r₁ z_ker_size Hx H_z_ker 2 := by
   intro I
   unfold n r₁ z_ker_size
   prep_smt
-  unfold H_z_ker H_x
+  unfold H_z_ker Hx
   --smt --observe: no error! accepted input!
   sorry
+-/
