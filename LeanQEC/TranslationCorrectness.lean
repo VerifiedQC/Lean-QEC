a337fcbe07cc84fd764e29d54cb83ac4cbabd7fd
Yi Lee <ylee1228@umd.edu>
2026-03-17 11:38:50 -0400

    Remove extraneous assumption

--- LeanQEC/TranslationCorrectness.lean
+++ LeanQEC/TranslationCorrectness.lean
@@ -10,7 +10,7 @@ def forget {a b : ℕ} [NeZero a] [NeZero b]
       M (Fin.ofNat a i) (Fin.ofNat b j) == 1
     else false)
 
-def kergen_correct {a b n : ℕ} [NeZero a] [NeZero b] [NeZero n]
+def kergen_correct {a b n : ℕ}
   (M : Matrix (Fin a) (Fin b) (ZMod 2))
   (gen : Fin n -> Fin b -> ZMod 2) : Prop :=
   LinearMap.ker M.toLin' =
@@ -20,17 +20,9 @@ theorem sat_translation_correct
   {n k₁ k₂ : ℕ} [NeZero n] [NeZero (n - k₁)] [NeZero (n - k₂)]
   (css : CSS_pair n k₁ k₂)
   (xkergen : Fin (n - k₁) → Fin n → ZMod 2)
-  (xkergen_correct :
-    let _ : NeZero k₁ := by
-      rcases css with ⟨_, _, _, _, _, _, _, gt0_k₁⟩
-      constructor; smt [gt0_k₁]
-    kergen_correct css.H₁ xkergen)
+  (xkergen_correct : kergen_correct css.H₁ xkergen)
   (zkergen : Fin (n - k₂) → Fin n → ZMod 2)
-  (zkergen_correct :
-    let _ : NeZero k₂ := by
-      rcases css with ⟨_, _, _, _, _, _, _, _, gt0_k₂⟩
-      constructor; smt [gt0_k₂]
-    kergen_correct css.H₂ zkergen)
+  (zkergen_correct : kergen_correct css.H₂ zkergen)
   (dist : ℕ) :
   let _ : NeZero (k₁ + k₂) := by
     rcases css with ⟨_, _, _, _, _, _, _, gt0_k₁⟩
