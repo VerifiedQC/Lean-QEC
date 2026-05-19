3b7349d48af17959c21ae161f2e7734338acac7d
mehatamm <mati@ehatamm.com>
2026-05-18 23:20:26 -0400

    change build targets

--- LeanQEC/Basic.lean
+++ LeanQEC/Basic.lean
@@ -1,5 +1,2 @@
 import LeanQEC.Stabilizer.BitVecSATToDist
 import LeanQEC.ComputerAlgebra.BitVecCorrectness
-import LeanQEC.Stabilizer.Examples.BB.BB90
-import LeanQEC.Stabilizer.Examples.BB.BB70
-import LeanQEC.Stabilizer.Examples.BB.GB54
